6e464d4e3163a84a1cf55a688a0d755e7ad433be
Mahedi-Hasan42 <mhmahedi42@gmail.com>
2025-05-06 23:19:03 +0600

    Add files via upload

---
+++ Parking/MODEL/dashboardconnection.php
@@ -0,0 +1,18 @@
+<?php
+class DashboardConnection {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $database = 'xton_airport';
+
+    public function getData() {
+        $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+    
+
+
+}
+?>
